f60c31a875ee0f0bf3bc44d81f70374bce521a4e
Taranpreet Multani <taran.multani@procaltech.com>
2024-01-28 18:51:36 -0500

    First commit

--- src/main/java/com/careerit/HelloWorld.java
+++ src/main/java/com/careerit/HelloWorld.java
@@ -2,6 +2,6 @@ package com.careerit;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello to the java World");
+        System.out.println("Hello to the ssss java World");
     }
 }
